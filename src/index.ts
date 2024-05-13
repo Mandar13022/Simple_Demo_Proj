8f054e34a0e55dbae1b945c39830a00c2ce4bcf2
Mandar13022 <112116535+Mandar13022@users.noreply.github.com>
2024-05-13 16:17:01 +0530

    Update index.ts
    a

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,4 @@ const PORT = process.env.PORT ?? 8000;
 
 app.use("/", router);
 
-app.listen(PORT, () => console.log(`Hello Server Started on PORT ${PORT} 🎉`));
+app.listen(PORT, () => console.log(`Hello AWS Server Started on PORT ${PORT} 🎉`));
